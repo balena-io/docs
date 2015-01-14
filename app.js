1ca8f053632b3d48759ba6e65d3f37143363da07
Milos Janjic <milosh012@gmail.com>
2015-01-14 10:36:17 +0100

    Added nodejs simple project for heroku.

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Server started on port ' + (process.env.PORT || 3000));
+});
+
+
