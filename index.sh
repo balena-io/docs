b5beb2ff4880baec2075f98d580034a8f7b810a0
Craig Mulligan <craig@firingsuad.co.za>
2015-04-16 15:04:11 +0100

    rename cli.md

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get latest CLI docs
-cd pages/using/ && { curl -O -L https://github.com/resin-io/resin-cli/raw/master/doc/cli.markdown ; cd -; }
+cd pages/using/ && { curl -O -L https://github.com/resin-io/resin-cli/raw/master/doc/cli.markdown ; mv cli.markdown cli.md ; cd -; }
 
 # build search index
 node index-builder.js
