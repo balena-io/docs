62dc4cf6af3b1e2a46eefbbd874f47a542b2a257
Craig Mulligan <craig@firingsuad.co.za>
2015-07-07 23:58:50 +0100

    rm first line of sdk doc & css fix

--- index.sh
+++ index.sh
@@ -4,6 +4,6 @@
 cd pages/using/ && { curl -O -L https://github.com/resin-io/resin-cli/raw/master/doc/cli.markdown ; mv cli.markdown cli.md ; cd -; }
 
 # get lastest SDK docs 
-cd pages/using/ && { curl -O -L https://github.com/resin-io/resin-sdk/raw/master/DOCUMENTATION.md ; mv DOCUMENTATION.md sdk.md ; cd -; }
+cd pages/using/ && { curl -O -L https://github.com/resin-io/resin-sdk/raw/master/DOCUMENTATION.md; tail -n +2 DOCUMENTATION.md > sdk.md ; rm DOCUMENTATION.md ; cd -; }
 # build search index
 node index-builder.js
