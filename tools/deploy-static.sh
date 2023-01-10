4a46e5a5666550a44d10e0daec381537ceb1f4c9
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2023-01-11 00:35:15 +0530

    patch: Enable trailing slash redirects
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- tools/deploy-static.sh
+++ tools/deploy-static.sh
@@ -14,10 +14,10 @@ DIVISION_LINE=`awk '/####*/ {print NR}' config/redirects.txt | sed -n 3p`
 head -n $DIVISION_LINE config/redirects.txt > build/_redirects
 
 # normalize all Cloudflare Style Redirects to end with /
-# sed -r 's/^(\/[^ ]+\/[^\/ ]+) /\1\/ /g' -i build/_redirects
+sed -r 's/^(\/[^ ]+\/[^\/ ]+) /\1\/ /g' -i build/_redirects
 
 # duplicate every Cloudflare Style Redirect ending with / to also end without /
-# sed -rn 'p; s/^(\/[^ ]+)\/ /\1 /p' -i build/_redirects
+sed -rn 'p; s/^(\/[^ ]+)\/ /\1 /p' -i build/_redirects
 
 # Copy the 404 page to root of the build folder for CF pages to find it
 cp build/404/index.html build/404.html
